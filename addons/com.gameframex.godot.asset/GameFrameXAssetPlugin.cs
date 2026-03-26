b865b5f9d3b22e28140239c5d877933bd4de1ab9
Blank <wangfj11@foxmail.com>
2026-03-26 16:50:31 +0800

    feat(asset): 添加 GameFrameX Asset 模块的 Godot 插件
    添加 Godot 编辑器插件文件，包括插件配置和基础类，用于集成 GameFrameX 资源管理模块到 Godot 编辑器中。

---
+++ addons/com.gameframex.godot.asset/GameFrameXAssetPlugin.cs
@@ -0,0 +1,12 @@
+#if TOOLS
+using Godot;
+
+namespace GameFrameX.Asset.Editor
+{
+    [Tool]
+    public partial class GameFrameXAssetPlugin : EditorPlugin
+    {
+        // 此模块暂无 InspectorPlugin / No InspectorPlugin for this module yet
+    }
+}
+#endif
