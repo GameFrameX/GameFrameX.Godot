559be0981471430dc6e46b6b8e9957e8b4efc18f
Blank <wangfj11@foxmail.com>
2026-03-26 16:49:54 +0800

    feat: 添加 GameFrameX GetChannel 插件基础文件
    添加插件配置文件、C# 脚本及 UID 文件，为 GameFrameX 渠道获取模块提供基础结构。

---
+++ addons/com.gameframex.godot.getchannel/GameFrameXGetChannelPlugin.cs
@@ -0,0 +1,12 @@
+#if TOOLS
+using Godot;
+
+namespace GameFrameX.GetChannel.Editor
+{
+    [Tool]
+    public partial class GameFrameXGetChannelPlugin : EditorPlugin
+    {
+        // 此模块暂无 InspectorPlugin / No InspectorPlugin for this module yet
+    }
+}
+#endif
