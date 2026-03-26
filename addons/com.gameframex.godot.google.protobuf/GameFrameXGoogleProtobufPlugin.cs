0aba79ece2119fc884b63832e266d6718df89caf
Blank <wangfj11@foxmail.com>
2026-03-26 16:49:28 +0800

    feat(protobuf): 添加 Google Protobuf 插件的基础文件
    添加 Godot 编辑器插件的基本结构文件，包括插件配置、C# 脚本及唯一标识文件，为后续集成 Google Protobuf 功能提供基础。

---
+++ addons/com.gameframex.godot.google.protobuf/GameFrameXGoogleProtobufPlugin.cs
@@ -0,0 +1,12 @@
+#if TOOLS
+using Godot;
+
+namespace GameFrameX.Protobuf.Editor
+{
+    [Tool]
+    public partial class GameFrameXGoogleProtobufPlugin : EditorPlugin
+    {
+        // 此模块暂无 InspectorPlugin / No InspectorPlugin for this module yet
+    }
+}
+#endif
